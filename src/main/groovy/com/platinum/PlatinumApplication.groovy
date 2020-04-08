577068db35235aed3455a72872e8cae9933b2c6b
iquinto <53236933+iquinto@users.noreply.github.com>
2020-04-08 15:14:11 +0200

    Add files via upload

---
+++ src/main/groovy/com/platinum/PlatinumApplication.groovy
@@ -0,0 +1,13 @@
+package com.platinum
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PlatinumApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PlatinumApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/platinum/PlatinumApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.platinum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlatinumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
